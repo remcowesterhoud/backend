2948bbc204be2f567414454af42b2cdc5507388a
RemcoW <remco.westerhoud@infosupport.com>
2016-12-14 14:22:18 +0100

    MatchDetails happy path test

---
+++ src/test/scala/com/leagueprojecto/api/services/MatchHistoryManagerTest.scala
@@ -0,0 +1,8 @@
+package com.leagueprojecto.api.services.riot
+
+/**
+  * Created by RemcoW on 14-12-2016.
+  */
+class MatchHistoryManagerTest {
+
+}
---
+++ src/test/scala/com/leagueprojecto/api/testHelpers/TestClass.scala
@@ -0,0 +1,8 @@
+package com.leagueprojecto.api.testHelpers
+
+/**
+  * Created by RemcoW on 14-12-2016.
+  */
+class TestClass {
+
+}
