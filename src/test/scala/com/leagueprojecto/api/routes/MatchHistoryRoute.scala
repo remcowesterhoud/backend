12bc5c9da30829c510b3cba3c9f610ebf1485a1a
RemcoW <remco.westerhoud@infosupport.com>
2017-01-03 10:09:30 +0100

    removed unused import

--- src/test/scala/com/leagueprojecto/api/routes/MatchHistoryRoute.scala
+++ src/test/scala/com/leagueprojecto/api/routes/MatchHistoryRoute.scala
@@ -2,7 +2,6 @@ package com.leagueprojecto.api.routes
 
 import com.analyzedgg.api.domain.riot.Player
 import com.analyzedgg.api.services.riot.SummonerService.SummonerNotFound
-import com.leagueprojecto.api.routes.RoutesTest
 
 // Do not remove the following import. IntelliJ might say it's not used, but it is for converting json to case classes.
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
