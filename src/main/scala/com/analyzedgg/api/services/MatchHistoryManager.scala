dd4b004b545891638e2e738f3c51256b3d6d8415
unknown <remcowesterhoud@msn.com>
2017-01-13 15:18:43 +0100

    Added match detail comments, without them it's pure magic what happens

--- src/main/scala/com/analyzedgg/api/services/MatchHistoryManager.scala
+++ src/main/scala/com/analyzedgg/api/services/MatchHistoryManager.scala
@@ -52,20 +52,27 @@ class MatchHistoryManager extends LazyLogging {
   }
 
   private def createGraph(bufferSize: Int = 100): RunnableGraph[SourceQueueWithComplete[GetMatches]] = {
-    // This Sink doesn't need to do anything with the elements as the reference to the objects is being tracked till they complete the Stream
+    // Sink wich will set the match details to the result variable of the request
     val returnSink = Sink.foreach(setResult)
+    // Start of the Stream, GetMatches requests can dynamically be passed to this Source
     val source = Source.queue[GetMatches](bufferSize, OverflowStrategy.backpressure)
 
     val lastIdsFlow = Flow.fromGraph(GraphDSL.create() { implicit builder =>
       // Import the implicits so the ~> syntax can be used
       import GraphDSL.Implicits._
 
+      // Flow that gets the latest match ids from the riot api
       val lastIdsFlow = builder.add(Flow[GetMatches].map(getLastIds).async)
+      // Flow that uses the match ids to try and get the details from the cache
       val detailsFromCacheFlow = builder.add(Flow[(GetMatches, Future[Seq[Long]])].map(getDetailsFromCache).async)
+      // Flow that uses the match ids to try and get the details from the riot api
       val detailsFromRiotFlow = builder.add(Flow[(GetMatches, Future[Map[Long, Option[MatchDetail]]])].map(getDetailsFromRiot).async)
+      // Broadcast that sends the match details to the cacheSink and the returnSink
       val detailsBroadcast = builder.add(Broadcast[(GetMatches, Future[Seq[MatchDetail]])](2))
+      // Sink that caches the details in the database
       val cacheDetailsSink = Sink.foreach(cacheDetails).async
 
+      // Chain the different components of the flow together
       lastIdsFlow ~> detailsFromCacheFlow ~> detailsFromRiotFlow ~> detailsBroadcast ~> cacheDetailsSink
 
       FlowShape(lastIdsFlow.in, detailsBroadcast.out(1))
@@ -75,32 +82,44 @@ class MatchHistoryManager extends LazyLogging {
 
   def getLastIds(data: GetMatches): (GetMatches, Future[Seq[Long]]) = {
     logger.info(s"Requesting last $matchAmount match ids from riot")
+    // Request the recent match ids from the riot api
     (data, service.getRecentMatchIds(data.region, data.summonerId, matchAmount))
   }
 
   def getDetailsFromCache(data: (GetMatches, Future[Seq[Long]])): (GetMatches, Future[Map[Long, Option[MatchDetail]]]) = {
-    (data._1, data._2.map(ids => ids.map(id => id -> None).toMap[Long, Option[MatchDetail]])
-      .map(matchesMap => matchesMap ++ repository.getDetailsAllowEmpty(matchesMap.keys.toSeq, data._1.region, data._1.summonerId)
+    (data._1, data._2.map(ids =>
+      // Parse the sequence of ids to a map of ids and optional MatchDetails
+      ids.map(id => id -> None).toMap[Long, Option[MatchDetail]])
+      .map(matchesMap =>
+        // Get the details that exist in the cache from there and put them in the map
+        matchesMap ++ repository.getDetailsAllowEmpty(matchesMap.keys.toSeq, data._1.region, data._1.summonerId)
           .map(detail => detail.matchId -> Some(detail))
       ))
   }
 
   def getDetailsFromRiot(data: (GetMatches, Future[Map[Long, Option[MatchDetail]]])): (GetMatches, Future[Seq[MatchDetail]]) = {
     (data._1, data._2.flatMap(matchesMap =>
+      // Filter the ids of which the match details were not available in the cache
       Future.sequence(matchesMap.filter(_._2.isEmpty).keys.toSeq.map(id =>
+        // Get the remaining match details
         service.getMatchDetails(data._1.region, data._1.summonerId, id))
+        // If the match details could be retrieved from the riot api pass them, else ignore them
         .map(_.map(Success(_)).recover({ case exception => Failure(exception) })))
         .map(_.collect({ case Success(succeededDetail) => succeededDetail }))
+        // Map the details that could be retrieved from riot to their corresponding ids in the map
         .map(details => details.map(detail => detail.matchId -> Some(detail)).toMap)
         .map(riotDetailMap => matchesMap ++ riotDetailMap)
+      // Remove the details that could not be retrieved from riot or the cache from the map
     ).map(detailsMap => detailsMap.values.toSeq.flatten))
   }
 
   def cacheDetails(data: (GetMatches, Future[Seq[MatchDetail]])): Unit = {
+    // Save the retrieved match details in the cache
     data._2.onSuccess({ case matchDetails => repository.save(matchDetails, data._1.region, data._1.summonerId) })
   }
 
   def setResult(data: (GetMatches, Future[Seq[MatchDetail]])): Unit = {
+    // Set the result variable of the request to contain a list of the most recent match details of the summoner
     data._1.result.completeWith(data._2)
   }
 }
--- src/main/scala/com/analyzedgg/api/services/riot/MatchService.scala
+++ src/main/scala/com/analyzedgg/api/services/riot/MatchService.scala
@@ -22,29 +22,33 @@ object MatchService {
 case class MatchService() extends RiotService with LazyLogging {
   def getRecentMatchIds(region: String, summonerId: Long, amount: Int): Future[Seq[Long]] = {
     val queryParams: Map[String, String] = Map("beginIndex" -> 0.toString, "endIndex" -> amount.toString)
+    // Try to retrieve the latest match ids from the riot api
     riotGetRequest(region, matchListBySummonerId + summonerId, queryParams).mapTo[HttpResponse].map(httpResponse =>
+      // If the ids could be retrieved map them ro RiotRecentMatches, else fail the future with an exception
       httpResponse.status match {
         case OK => mapRiotTo(httpResponse.entity, classOf[RiotRecentMatches])
         case NotFound => throw FailedRetrievingRecentMatches
         case _ => throw new RuntimeException(s"An unknown error occurred. riot API response:\n$httpResponse")
-      }
-    ).flatMap(futureMatches => futureMatches.map(recentMatchList =>
-      if (recentMatchList.matches.nonEmpty) {recentMatchList.matches.map(_.matchId) }
-      else throw NoRecentMatchesPlayed))
+      })
+      // Check if the summoner played any matches recently, if he did return their ids, else fail the future
+      .flatMap(futureMatches => futureMatches.map(recentMatchList =>
+        if (recentMatchList.matches.nonEmpty) recentMatchList.matches.map(_.matchId)
+        else throw NoRecentMatchesPlayed))
   }
 
 
   def getMatchDetails(region: String, summonerId: Long, matchId: Long): Future[MatchDetail] = {
+    // Try to retrieve the match details from the riot api
     riotGetRequest(region, matchById + matchId).mapTo[HttpResponse].map(httpResponse => {
+      // If the details could be retrieved map it to RiotMatch, else fail the future with an exception
       httpResponse.status match {
         case OK => mapRiotTo(httpResponse.entity, classOf[RiotMatch]).mapTo[RiotMatch]
         case _ =>
           logger.error(s"Failed retrieving match details.\nReason: $httpResponse")
           throw FailedRetrievingMatchDetails
-      }
-    }).flatMap(futureRiotMatch => {
-      futureRiotMatch.map(toMatchDetail(_, summonerId))
-    })
+      }})
+      // Map the RiotMatch to a MatchDetail
+      .flatMap(futureRiotMatch => futureRiotMatch.map(toMatchDetail(_, summonerId)))
   }
 
   private[this] def toMatchDetail(riotMatch: RiotMatch, summonerId: Long): MatchDetail = {
